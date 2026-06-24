c033283e2a4e85230294f00c64be006e75e9fbc1
Sebastian Rios <sebastian.g.rios@gmail.com>
2026-06-24 12:20:28 -0300

    Se corrige vinculacion con account.move.line

--- models/account_move_line.py
+++ models/account_move_line.py
@@ -4,34 +4,32 @@ from odoo import api, fields, models
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    product_auth_warning = fields.Char(
-        compute="_compute_product_auth_warning",
-        string="Autorización producto",
+    product_id_domain = fields.Binary(
+        compute="_compute_product_id_domain",
+        store=False,
     )
 
-    @api.depends(
-        "product_id",
-        "move_id.partner_id",
-        "move_id.move_type",
-    )
-    def _compute_product_auth_warning(self):
+    @api.depends("move_id.partner_id", "move_id.move_type")
+    def _compute_product_id_domain(self):
         SupplierProduct = self.env["aerotec.supplier.product"].sudo()
-        config_cache = {}
+        partner_domain_cache = {}
         for line in self:
-            line.product_auth_warning = False
             if (
                 line.move_id.move_type != "in_invoice"
-                or not line.product_id
                 or not line.move_id.partner_id
             ):
+                line.product_id_domain = []
                 continue
-            key = line.move_id.partner_id.id
-            if key not in config_cache:
+            partner_id = line.move_id.partner_id.id
+            if partner_id not in partner_domain_cache:
                 config = SupplierProduct.search(
-                    [("partner_id", "=", key)],
+                    [("partner_id", "=", partner_id)],
                     limit=1,
                 )
-                config_cache[key] = config.product_ids if config else None
-            authorized = config_cache[key]
-            if authorized is not None and line.product_id.product_tmpl_id not in authorized:
-                line.product_auth_warning = "Producto no autorizado para este proveedor"
+                if config and config.product_ids:
+                    partner_domain_cache[partner_id] = [
+                        ("product_tmpl_id", "in", config.product_ids.ids)
+                    ]
+                else:
+                    partner_domain_cache[partner_id] = []
+            line.product_id_domain = partner_domain_cache[partner_id]
